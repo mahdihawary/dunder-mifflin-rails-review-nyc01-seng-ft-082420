4c6f8a422dc17fc50e65b9c7c90334a06ab17be3
mahdihawary <Mahdihawary@gmail.com>
2020-09-17 15:12:08 -0400

    first commit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,25 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: :show
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        
+    end
+
+
+
+
+
+        private
+    def set_dog
+        @dog = Dog.find(params[:id])
+    end
+    
+    def dog_params(*args)
+        params.require(:dog).permit(*args)
+    end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,25 @@
 class EmployeesController < ApplicationController
+    before_action :set_employee, only: :show
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        
+    end
+
+    
+
+
+
+        private
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
+    
+    def employee_params(*args)
+        params.require(:employee).permit(*args)
+    end
+
 end
